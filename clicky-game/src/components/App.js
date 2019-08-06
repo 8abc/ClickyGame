0a5f5d222f67c6835f7f2f8980779b01ddf7e9d0
8abc <anadelaine.catalla@gmail.com>
2019-08-05 21:22:24 -0700

    sort functionality is working

--- clicky-game/src/components/App.js
+++ clicky-game/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Card from "./Card";
-// import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 import Container from "./Container";
 // import Footer from "./Footer";
 import cards from "./cards.json";
@@ -22,7 +22,7 @@ class App extends Component {
     this.state.cards.forEach(card => {
       card.count = 0;
     });
-    alert(`Gave Over:( \nscore: ${this.state.score})`);
+    alert(`Gave Over\nscore: ${this.state.score}`);
     this.setState({ score: 0 });
     return true;
   };
@@ -35,7 +35,7 @@ class App extends Component {
           this.setState({ score: this.state.score + 1 }, function() {
             console.log(this.state.score);
           });
-          this.state.card.sort(() => Math.random() - 0.5);
+          this.state.cards.sort(() => Math.random() - 0.5);
           return true;
         } else {
           this.gameOver();
@@ -47,9 +47,9 @@ class App extends Component {
   render() {
     return (
       <Container>
-        {/* <Navbar score={this.state.score} highscore={this.state.highscore}>
+        <Navbar score={this.state.score} highscore={this.state.highscore}>
           Clicky Game
-        </Navbar> */}
+        </Navbar>
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -3,10 +3,8 @@ import "./style.css";
 
 const Navbar = prop => (
   <div className="navbar">
-    <div className="title">{props.children}</div>
-    <div className="scores">
-      Score: {props.score} | Highscore: {props.highscore}
-    </div>
+    {/* <div className="title">{props.children}</div> */}
+    <div className="scores">Score: | Highscore:</div>
   </div>
 );
 
