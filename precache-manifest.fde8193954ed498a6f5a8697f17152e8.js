013f43d0708d7eae8a89d5029462177cba627962
8abc <anadelaine.catalla@gmail.com>
2019-08-07 18:00:35 -0700

    Updates

---
+++ precache-manifest.fde8193954ed498a6f5a8697f17152e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f012683d5d335318c75b972e4284848",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "9f6254da835e11a4c20b",
+    "url": "/ClickyGame/static/css/main.3ab4b19a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9f6254da835e11a4c20b",
+    "url": "/ClickyGame/static/js/main.620cd3a4.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
